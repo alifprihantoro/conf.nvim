c2de6f9ce9b44c8fd728c429988fe9a28347ec27
alifprihantoro <alifmuryp@gmail.com>
2022-12-22 15:12:25 +0700

    fixed: delete not use

--- lua/myapps/git/maps.lua
+++ lua/myapps/git/maps.lua
@@ -3,15 +3,11 @@ local gitPush = require('myapps.git.function.gsv')
 local gitCommit = require('myapps.git.function.gsv')
 
 local key = vim.keymap.set
-local cmd = vim.api.nvim_create_user_command
 local opt = { silent = true }
 
 key('i', '<leader>gg', gitSave, opt)
 key('i', '<leader>gp', gitPush, opt)
-key('i', '<leader>gc', gitCommit, opt)
 key('i', '<leader>gf', ':Telescope git_files<CR>', opt)
 key('i', '<leader>gb', ':Telescope git_branch<CR>', opt)
 key('i', '<leader>gc', ':Telescope git_commits<CR>', opt)
 key('i', '<leader>gs', ':Telescope git_status<CR>', opt)
-
-cmd('GitCheck')
