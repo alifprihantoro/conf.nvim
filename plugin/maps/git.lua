a5ddfdd6cc840f42764eedf4519f3e1f1abd084c
alifprihantoro <alifmuryp@gmail.com>
2023-01-10 11:08:11 +0700

    fix: delete git flow func and note

--- plugin/maps/git.lua
+++ plugin/maps/git.lua
@@ -1,8 +1,4 @@
--- NOTE :
--- > git flow
--- > git merge
 local key = vim.keymap.set
-
 
 local gitCommitCmd = " && cd $(git rev-parse --show-toplevel) && git add . && git commit"
 local gitPush = " && gssh && git pushall"
@@ -16,17 +12,9 @@ local function gitPushCommit()
   vim.cmd("!tmux popup -E 'cd " .. GET_PATH_NOW .. gitCommitCmd .. gitPush .. "'")
 end
 
-local function gitFlow()
-  NAME_BRANC_BEFORE = vim.cmd('echo system("echo $(git symbolic-ref --short HEAD)")')
-  -- vim.cmd('Telescope git_branches')
-  NAME_BRANC_NOW = vim.cmd('echo system("echo $(git symbolic-ref --short HEAD)")')
-  print('hello :')
-  print(NAME_BRANC_NOW)
-end
 -- normal
 key('n', '<leader>gs', ':Telescope git_status<CR>')
 key('n', '<leader>gc', ':Telescope git_branches<CR>')
-key('n', '<leader>gf', gitFlow)
+key('n', '<leader>gf', ':Telescope git_flow<CR>')
 key('n', '<leader>gv', gitCommit)
 key('n', '<leader>gp', gitPushCommit)
-key('n', '<leader>gf', gitFlow)
