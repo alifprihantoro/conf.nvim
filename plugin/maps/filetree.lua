d1c587fa2023f898495956d165722b57dc207491
alifprihantoro <alifmuryp@gmail.com>
2023-01-11 12:06:17 +0700

    fix: delete map not use

--- plugin/maps/filetree.lua
+++ plugin/maps/filetree.lua
@@ -1,7 +1,6 @@
 local key = vim.keymap.set
 local opts = { silent = true }
 -- FILE TREE
-key('n', '<C-E>', ':NvimTreeToggle<CR>',opts)
 key('n', '<leader>ee', ':NvimTreeOpen<CR>',opts)
 key('n', '<leader>eg',
   ":exec 'NvimTreeOpen' fnameescape(fnamemodify(finddir('.git', escape(expand('%:p:h'), ' ') . ';'), ':h'))<CR>",opts)
