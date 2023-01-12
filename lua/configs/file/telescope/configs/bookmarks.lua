6d3ebb38ff7ea5255133255408950f3b057669e0
alifprihantoro <alifmuryp@gmail.com>
2023-01-12 10:26:49 +0700

    fix: telescope bookmarks

--- lua/configs/file/telescope/configs/bookmarks.lua
+++ lua/configs/file/telescope/configs/bookmarks.lua
@@ -1,4 +1,10 @@
-return {
+local M = {}
+M.list_bookmark = {
   muryp = "~/web/muryp",
   vim = "~/.config/nvim",
 }
+M.option = {
+  treeOpen = 'lua _G.TreeOpenNow()',
+  telescope = 'Telescope find_files'
+}
+return M
--- lua/configs/file/telescope/plugins/bookmark.lua
+++ lua/configs/file/telescope/plugins/bookmark.lua
@@ -1,13 +1,18 @@
-local picker = require "configs.file.telescope.utils.picker"
-local list_bookmark = require "configs.file.telescope.configs.bookmarks"
+local picker   = require "configs.file.telescope.utils.picker"
+local opts     = require "configs.file.telescope.configs.bookmarks"
+local getNames = require "configs.file.telescope.utils.getNames"
 
 return function()
-  local names = {}
-  for k in pairs(list_bookmark) do
-    table.insert(names, k)
-  end
+  local names = getNames(opts.list_bookmark)
   local function callback(selection)
-    vim.cmd('NvimTreeToggle ' .. list_bookmark[selection[1]])
+    local opts_names = getNames(opts.option)
+    picker({
+      opts = opts_names,
+      callBack = function(selection2)
+        vim.cmd('cd ' .. opts.list_bookmark[selection[1]])
+        vim.cmd('execute "' .. opts.option[selection2[1]] .. '"')
+      end
+    })
   end
 
   picker({
