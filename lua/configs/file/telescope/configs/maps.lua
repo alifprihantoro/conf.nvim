555bf38c94c9ea6cb4dc08eba9427c102069c070
alifprihantoro <alifmuryp@gmail.com>
2023-01-14 14:37:12 +0700

    fix: change maps

--- lua/configs/file/telescope/configs/maps.lua
+++ lua/configs/file/telescope/configs/maps.lua
@@ -6,11 +6,12 @@ return {
     ["<c-k>"] = actions.move_selection_previous,
     ["<c-p>"] = actions.preview_scrolling_up,
     ["<c-n>"] = actions.preview_scrolling_down,
+    ["<C-f>"] = require('telescope.actions.layout').toggle_preview,
     ["<c-u>"] = false,
     ["<c-l>"] = false,
-    ['<C-p>'] = require('telescope.actions.layout').toggle_preview
   },
   n = {
     ["q"] = actions.close,
+    ["<leader>p"] = require('telescope.actions.layout').toggle_preview
   }
 }
