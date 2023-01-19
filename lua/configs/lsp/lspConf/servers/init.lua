9238585c6beae68690588cf55df47d12984950a2
alifprihantoro <alifmuryp@gmail.com>
2023-01-19 23:14:38 +0700

    perf: disable not use but maybe use in future

--- lua/configs/lsp/lspConf/servers/init.lua
+++ lua/configs/lsp/lspConf/servers/init.lua
@@ -2,7 +2,7 @@ local capability   = require('configs.lsp.lspConf.utils.capability')
 local lua          = require('configs.lsp.lspConf.servers.lua')
 local lsp          = require('lspconfig')
 local jsonls       = require('configs.lsp.lspConf.servers.jsonls')
-local css          = require('configs.lsp.lspConf.servers.css')
+-- local css          = require('configs.lsp.lspConf.servers.css')
 local yaml         = require('configs.lsp.lspConf.servers.yaml')
 local rust         = require('configs.lsp.lspConf.servers.rust')
 local capabilities = { capabilities = capability }
@@ -12,10 +12,10 @@ lsp.sumneko_lua.setup(lua) -- lua
 lsp.tsserver.setup { capabilities } -- js typescript
 lsp.html.setup(capabilities)
 lsp.jsonls.setup(jsonls)
-lsp.cssls.setup(css)
-lsp.cssmodules_ls.setup {}
+-- lsp.cssls.setup(css)
+-- lsp.cssmodules_ls.setup {}
 lsp.eslint.setup {}
-require('lspconfig').yamlls.setup(yaml)
+lsp.yamlls.setup(yaml)
 lsp.bashls.setup(capabilities)
 lsp.pyright.setup {}
 lsp.rust_analyzer.setup(rust)
