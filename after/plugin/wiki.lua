8fd8d2377eb2e4a661800395fb460c0507608622
alifprihantoro <alifmuryp@gmail.com>
2023-03-28 15:08:20 +0700

    fix: wiki active

--- after/plugin/wiki.lua
+++ after/plugin/wiki.lua
@@ -1,9 +1,10 @@
 -- active wiki in other ext
-vim.api.nvim_cmd('WikiActive', function()
+vim.api.nvim_create_user_command('WikiActive', function()
   local origin_file_type = vim.bo.filetype
   local cmd = [[
     set filetype=markdown
     set filetype=vimwiki
   ]] .. 'set filetype=' .. origin_file_type
   vim.cmd(cmd)
-end,{})
+  require('configs.editor.wiki.mapping')()
+end, {})
