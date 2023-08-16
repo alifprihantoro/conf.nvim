40aa89b7cc7d4a33dacea1013a25f00207267c2a
alifprihantoro <alifprihantoro@gmail.com>
2023-08-16 19:44:30 +0700

    change sidebar

--- lua/configs/helper/ufo.lua
+++ lua/configs/helper/ufo.lua
@@ -1,4 +1,4 @@
-vim.o.foldcolumn = '1'  -- '0' is not bad
+vim.o.foldcolumn = '0'
 vim.o.foldlevel = 99    -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
@@ -34,7 +34,7 @@ end
 
 require('ufo').setup({
   provider_selector = function(bufnr, filetype, buftype)
-    return {'treesitter', 'indent' }
+    return { 'treesitter', 'indent' }
   end,
   fold_virt_text_handler = handler,
   fold_last_character = true,
