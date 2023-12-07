a6629b4e5fb9c697ad6fde44a5c2956eae24d33a
alifprihantoro <alifprihantoro@gmail.com>
2023-12-07 13:40:05 +0700

    refactor: mv lsp conf and wichkey conf into after plugins

---
+++ after/plugin/config/lsp/ui/hi_line_number.lua
@@ -0,0 +1,6 @@
+vim.cmd [[
+  highlight! DiagnosticLineNrError guibg=#51202A guifg=#FF0000 gui=bold
+  highlight! DiagnosticLineNrWarn guibg=#51412A guifg=#FFA500 gui=bold
+  highlight! DiagnosticLineNrInfo guibg=#1E535D guifg=#00FFFF gui=bold
+  highlight! DiagnosticLineNrHint guibg=#1E205D guifg=#0000FF gui=bold
+]]
