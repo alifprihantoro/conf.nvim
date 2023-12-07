f1652d6ba6a41ac39fc7107c40d4c34708d23d70
alifprihantoro <alifprihantoro@gmail.com>
2023-12-07 19:22:46 +0700

    refactor: delete not use

---
+++ plugin/opts.lua
@@ -0,0 +1,53 @@
+-- vim.cmd("autocmd!")
+local set          = vim.opt
+set.swapfile       = false
+set.background     = 'dark'
+set.compatible     = false
+vim.scriptencoding = 'utf-8'
+set.spelllang      = { "en", 'id' }
+set.encoding       = 'utf-8'
+set.fileencoding   = 'utf-8'
+vim.wo.number      = true
+set.title          = true
+set.autoindent     = true
+set.smartindent    = true
+set.hlsearch       = true
+set.backup         = false
+set.showcmd        = true
+set.showtabline    = 0
+set.cmdheight      = 0
+set.laststatus     = 0
+set.expandtab      = true
+set.scrolloff      = 10
+set.shell          = 'zsh'
+set.backupskip     = { '/tmp/*', '/private/tmp/*' }
+set.inccommand     = 'split'
+set.ignorecase     = true -- Case insensitive searching UNLESS /C or capital in search
+set.smarttab       = true
+set.breakindent    = true
+set.shiftwidth     = 2
+set.tabstop        = 2
+set.wrap           = false -- No Wrap lines
+set.backspace      = { 'start', 'eol', 'indent' }
+set.wildignore:append { '*/node_modules/*' }
+-- enable line and column display
+set.ruler        = true
+-- scroll a bit horizontally when at the end of the line
+set.sidescroll   = 6
+-- Make it easier to work with buffers
+-- http://vim.wikia.com/wiki/Easier_buffer_switching
+set.hidden       = true
+set.confirm      = true
+-- open new split panes to right and below (as you probably expect)
+set.splitright   = true
+set.splitbelow   = true
+vim.o.lazyredraw = false
+set.conceallevel = 1
+vim.cmd [[
+autocmd TermOpen * setlocal nonumber norelativenumber
+autocmd TermOpen term://* startinsert
+" syntax highlight
+syntax on
+au BufEnter *.html set syntax=html
+au BufEnter *.mdx set filetype=markdown.mdx
+]]
