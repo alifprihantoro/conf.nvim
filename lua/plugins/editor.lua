c60befcd76299905092f056fcff77f4ea3a82565
alifprihantoro <alifprihantoro@gmail.com>
2023-12-09 15:07:11 +0700

    refactor: todo

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,9 +1,22 @@
+local todoComment = require "configs.editor.todoComment"
+
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 return {
   {
+    'nvim-pack/nvim-spectre',
+    build = false,
+    cmd = 'Spectre',
+    opts = { open_cmd = 'noswapfile vnew' },
+    -- stylua: ignore
+    keys = {
+      { "<leader>Sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+    },
+  },
+  {
     'nvim-telescope/telescope.nvim',
+    Lazy = true,
     cmd = 'Telescope',
     config = function()
       require 'configs.editor.telescope'
@@ -15,7 +28,7 @@ return {
   },
   {
     'nvim-neo-tree/neo-tree.nvim',
-    event = 'BufWinEnter',
+    Lazy = true,
     cmd = 'Neotree',
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -28,82 +41,11 @@ return {
   },
   {
     'muryp/muryp-git-setup.nvim',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'folke/which-key.nvim',
-      'muryp/muryp-gh.nvim',
-      'lewis6991/gitsigns.nvim',
-      'nvim-lua/plenary.nvim',
-      'sindrets/diffview.nvim',
-      'akinsho/git-conflict.nvim',
-    },
+    import = 'muryp-git-setup/plugin'
   },
   {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      signs = true,      -- show icons in the signs column
-      sign_priority = 8, -- sign priority
-      -- keywords recognized as todo comments
-      keywords = {
-        FIX = {
-          icon = ' ', -- icon used for the sign, and in search results
-          color = 'error', -- can be a hex color, or a named color (see below)
-          alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
-          -- signs = false, -- configure signs for some keywords individually
-        },
-        TODO = { icon = ' ', color = 'info' },
-        HACK = { icon = ' ', color = 'warning' },
-        WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
-        PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-        NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
-        TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
-      },
-      gui_style = {
-        fg = 'NONE',         -- The gui style to use for the fg highlight group.
-        bg = 'BOLD',         -- The gui style to use for the bg highlight group.
-      },
-      merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-      -- highlighting of the line containing the todo comment
-      -- * before: highlights before the keyword (typically comment characters)
-      -- * keyword: highlights of the keyword
-      -- * after: highlights after the keyword (todo text)
-      highlight = {
-        multiline = true,         -- enable multine todo comments
-        multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
-        multiline_context = 10,   -- extra lines that will be re-evaluated when changing a line
-        before = '',              -- "fg" or "bg" or empty
-        keyword = 'wide',
-        after = 'fg',                    -- "fg" or "bg" or empty
-        pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
-        comments_only = true,            -- uses treesitter to match keywords in comments only
-        max_line_len = 400,              -- ignore lines longer than this
-        exclude = {},                    -- list of file types to exclude highlighting
-      },
-      -- list of named colors where we try to extract the guifg from the
-      -- list of highlight groups or use the hex color if hl not found as a fallback
-      colors = {
-        error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
-        warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
-        info = { 'DiagnosticInfo', '#2563EB' },
-        hint = { 'DiagnosticHint', '#10B981' },
-        default = { 'Identifier', '#7C3AED' },
-        test = { 'Identifier', '#FF00FF' },
-      },
-      search = {
-        command = 'rg',
-        args = {
-          '--color=never',
-          '--no-heading',
-          '--with-filename',
-          '--line-number',
-          '--column',
-        },
-        -- regex that will be used to match keywords.
-        -- don't replace the (KEYWORDS) placeholder
-        pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-        -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-      },
-    },
+    opts = todoComment,
   },
 }
