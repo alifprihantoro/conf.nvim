23f14907b0b8be7f50b5eedb30d817339a844e64
alifprihantoro <alifprihantoro@gmail.com>
2023-12-25 17:26:31 +0700

    fix: indent markdown and add some opts

--- plugin/opts.lua
+++ plugin/opts.lua
@@ -11,24 +11,26 @@ vim.wo.number = true
 set.title = true
 set.autoindent = true
 set.smartindent = true
+set.smarttab = true
+set.backspace = { 'start', 'eol', 'indent' }
+set.shiftround = true -- Round indent
+set.softtabstop = 2
+set.shiftwidth = 2
+set.tabstop = 2
+set.expandtab = true
 set.hlsearch = true
 set.backup = false
 set.showcmd = true
 set.showtabline = 0
 set.cmdheight = 0
 set.laststatus = 0
-set.expandtab = true
 set.scrolloff = 10
 set.shell = 'zsh'
 set.backupskip = { '/tmp/*', '/private/tmp/*' }
 set.inccommand = 'split'
 set.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
-set.smarttab = true
 set.breakindent = true
-set.shiftwidth = 2
-set.tabstop = 2
 set.wrap = false -- No Wrap lines
-set.backspace = { 'start', 'eol', 'indent' }
 set.wildignore:append { '*/node_modules/*' }
 -- enable line and column display
 set.ruler = true
@@ -43,6 +45,17 @@ set.splitright = true
 set.splitbelow = true
 vim.o.lazyredraw = false
 set.conceallevel = 1
+set.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+set.wildmode = "longest:full,full" -- Command-line completion mode
+set.winminwidth = 5 -- Minimum window width
+set.termguicolors = true -- True color support
+set.timeoutlen = 300
+set.undofile = true
+set.undolevels = 10000
+set.shortmess:append({ W = true, I = true, c = true, C = true })
+set.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+set.smartcase = true -- Don't ignore case with capitals
+vim.g.markdown_recommended_style = 0
 vim.cmd [[
 autocmd TermOpen * setlocal nonumber norelativenumber
 autocmd TermOpen term://* startinsert
