d3ef58fd7ee3c0020de6d00c8aadb7f5dd068f2f
alifprihantoro <alifprihantoro@gmail.com>
2024-06-25 19:45:23 +0700

    update configs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,10 @@ return {
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     build = 'cd app && pnpm install',
     init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_filetypes = { 'markdown', 'html' }
+      vim.g.mkdp_combine_preview = 1
+      vim.g.mkdp_combine_preview_auto_refresh = 1
     end,
     ft = { 'markdown' },
   },
